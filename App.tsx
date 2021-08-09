5016462767f81d89a6bd2b6c5b8d0a799481fbf8
emilj <emil.jessen94@gmail.com>
2021-08-09 20:23:03 +0200

    init

--- App.tsx
+++ App.tsx
@@ -1,21 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text } from 'react-native';
+import Frontpage from './src/Frontpage';
+import NewGame from './src/screens/NewGame';
+import * as Screens from './src/global/constants';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import PreviousGames from './src/screens/PreviousGames';
+import Game from './src/screens/Game';
+import Games from './src/screens/Games';
+import { StackNavigatorParams } from './src/data/types';
 
-export default function App() {
+
+const { Screen, Navigator } = createStackNavigator<StackNavigatorParams>();
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <View style={{ flex: 1, backgroundColor: 'white' }}>
+        <Navigator
+          initialRouteName={Screens.Frontpage}
+          headerMode='none'
+        >
+          <Screen name={Screens.Frontpage} component={Frontpage} />
+          <Screen name={Screens.NewGame} component={NewGame} />
+          <Screen name={Screens.PreviousGames} component={PreviousGames} />
+          <Screen name={Screens.Game} component={Game} />
+          <Screen name={Screens.Games} component={Games} />
+        </Navigator>
+      </View>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
